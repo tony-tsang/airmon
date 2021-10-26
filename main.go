3317991fdc89ec29464c88116639e4f61b20d233
Tony Tsang <tcmtsang@gmail.com>
2021-10-26 21:55:14 +0800

    select on channels

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
     htu31 "airmon/htu31"
-    pmsa003i "airmon/pmsa003i"
     "airmon/metrics"
+    pmsa003i "airmon/pmsa003i"
     "log"
     "time"
 )
@@ -21,17 +21,28 @@ func main() {
     go metrics.StartServer()
 
     for {
-        tempHumidity := <-tempHumidityChannel
-        pmValue := <-pmValueChannel
-        log.Printf("Temperature %.2f, humidity %.2f\n", tempHumidity.Temp, tempHumidity.Humidity)
-        log.Printf("PM1.0 %d PM2.5 %d PM10 %d", pmValue.PM10std, pmValue.PM25std, pmValue.PM100std)
+        select {
+            case tempHumidity := <-tempHumidityChannel:
+                log.Printf("Temperature %.2f, humidity %.2f\n", tempHumidity.Temp, tempHumidity.Humidity)
+                metrics.TemperatureMetric.Set(tempHumidity.Temp)
+                metrics.HumidityMetric.Set(tempHumidity.Humidity)
+
+            case pmValue := <-pmValueChannel:
+                log.Printf("PM1.0 %d PM2.5 %d PM10 %d", pmValue.PM10std, pmValue.PM25std, pmValue.PM100std)
+                metrics.PM10StdMetric.Set(float64(pmValue.PM10std))
+                metrics.PM25StdMetric.Set(float64(pmValue.PM25std))
+                metrics.PM100StdMetric.Set(float64(pmValue.PM100std))
+            default:
+                time.Sleep(1 * time.Second)
+        }
+
+
+
 
         //metrics := grafana_cloud.Metrics{TempHumidity: tempHumidity, PMValues: pmValue}
 
-        metrics.TemperatureMetric.Set(tempHumidity.Temp)
-        metrics.HumidityMetric.Set(tempHumidity.Humidity)
-        metrics.PM10StdMetric.Set(float64(pmValue.PM10std))
-        metrics.PM25StdMetric.Set(float64(pmValue.PM25std))
-        metrics.PM100StdMetric.Set(float64(pmValue.PM100std))
+
+
+
     }
 }
