f784d3891163a831154ac2ee907c2e688470d378
Tony Tsang <tcmtsang@gmail.com>
2022-08-27 22:00:31 +0800

    functional uc8159

--- cmd/spi_test/main.go
+++ cmd/spi_test/main.go
@@ -6,7 +6,7 @@ import (
     // "time"
     "periph.io/x/host/v3"
     "periph.io/x/conn/v3/physic"
-    "periph.io/x/conn/v3/driver/driverreg"
+    // "periph.io/x/conn/v3/driver/driverreg"
     "periph.io/x/conn/v3/spi"
     "periph.io/x/conn/v3/spi/spireg"
     // "periph.io/x/conn/v3/i2c"
@@ -21,22 +21,27 @@ import (
 )
 
 func main() {
-	_, err := host.Init()
+	state, err := host.Init()
     if err != nil {
 		log.Fatalf("failed to initialize periph: %v", err)
 	}
 
-    _, err = driverreg.Init()
-    if err != nil {
-        log.Fatalf("failed to initialize periph: %v", err)
+    log.Printf("Using drivers:\n")
+    for _, driver := range state.Loaded {
+        log.Printf("- %s", driver)
     }
 
-    spi_bus, err := spireg.Open("spi0")
+    // _, err = driverreg.Init()
+    // if err != nil {
+    //     log.Fatalf("failed to initialize periph: %v", err)
+    // }
+
+    spi_bus, err := spireg.Open("")
     if err != nil {
         log.Fatalf("failed to open SPI: %v", err)
     }
 
-    i2c_bus, err := i2creg.Open("0")
+    i2c_bus, err := i2creg.Open("")
     if err != nil {
         log.Fatalf("failed to open I2C: %v", err)
     }
@@ -44,7 +49,14 @@ func main() {
     defer spi_bus.Close()
     defer i2c_bus.Close()
     
-    spi_channel, err := spi_bus.Connect(physic.MegaHertz, spi.Mode3, 8)
+    spi_channel, err := spi_bus.Connect(physic.MegaHertz, spi.Mode3 | spi.NoCS, 8)
+
+    if p, ok := spi_channel.(spi.Pins); ok {
+        log.Printf("  CLK : %s", p.CLK())
+		log.Printf("  MOSI: %s", p.MOSI())
+		log.Printf("  MISO: %s", p.MISO())
+		log.Printf("  CS  : %s", p.CS())
+    }
 
 	d := new(uc8159.Display)
 	d.Init(spi_channel)
--- internal/pkg/uc8159/uc8159.go
+++ internal/pkg/uc8159/uc8159.go
@@ -12,8 +12,6 @@ import (
 const Width = 640
 const Height = 400
 
-const Spi_Chunk_Size = 4096
-
 const (
 	RESET_PIN = "GPIO27"
 	BUSY_PIN = "GPIO17"
@@ -27,12 +25,17 @@ const (
 )
 
 const (
+	SPI_CHUNK_SIZE = 4096
+)
+
+const (
 	PSR = 0x00
 	PWR = 0x01
 	POF = 0x02
 	PFS = 0x03
 	PON = 0x04
 	DTM1 = 0x10
+	DSP = 0x11
 	DRF = 0x12
 	IPC = 0x13
 	PLL = 0x30
@@ -62,6 +65,7 @@ type Display struct {
 	busy gpio.PinIO
 	dc gpio.PinIO
 	cs gpio.PinIO
+	gpioInit bool
 }
 
 func (d *Display) Width() uint16 {
@@ -74,51 +78,121 @@ func (d *Display) Height() uint16 {
 
 func (d *Display) Init(spiBus spi.Conn) {
 	
-	d.buffer = make([]byte, bufSz)
-	d.spiBus = spiBus
+	d.buffer = make([]byte, bufSz, bufSz)
+ 	d.spiBus = spiBus
 
-	d.reset = gpioreg.ByName(RESET_PIN)
-	if d.reset == nil {
-		log.Fatalf("Unable to find %s", RESET_PIN)
-	}
-	d.reset.Out(gpio.High)
-
-	d.dc = gpioreg.ByName(DC_PIN)
-	if d.dc == nil {
-		log.Fatalf("Unable to find %s", DC_PIN)
-	}
-	d.dc.Out(gpio.Low)
-
-	d.cs = gpioreg.ByName(CS0_PIN)
-	if d.cs == nil {
-		log.Fatalf("Unable to find %s", CS0_PIN)
-	}
-	d.cs.Out(gpio.High)
-
-	d.busy = gpioreg.ByName(BUSY_PIN)
-	if d.busy == nil {
-		log.Fatalf("Unable to find %s", BUSY_PIN)
-	}
-	d.busy.In(gpio.PullDown, gpio.NoEdge)
-
-	d.Reset()
 }
 
-func (d *Display) Reset() {
+func (d *Display) setup() {
+
+	if !d.gpioInit {
+
+		d.reset = gpioreg.ByName(RESET_PIN)
+		if d.reset == nil {
+			log.Fatalf("Unable to find %s", RESET_PIN)
+		}
+		d.reset.Out(gpio.High)
+
+		d.dc = gpioreg.ByName(DC_PIN)
+		if d.dc == nil {
+			log.Fatalf("Unable to find %s", DC_PIN)
+		}
+		d.dc.Out(gpio.Low)
+
+		d.cs = gpioreg.ByName(CS0_PIN)
+		if d.cs == nil {
+			log.Fatalf("Unable to find %s", CS0_PIN)
+		}
+		d.cs.Out(gpio.High)
+
+		d.busy = gpioreg.ByName(BUSY_PIN)
+		if d.busy == nil {
+			log.Fatalf("Unable to find %s", BUSY_PIN)
+		}
+		d.busy.In(gpio.PullDown, gpio.NoEdge)
+
+		d.gpioInit = true
+	}
+
 	d.reset.Out(gpio.Low)
 	time.Sleep(100 * time.Millisecond)
 	d.reset.Out(gpio.High)
 
+	d.busyWait(1000)
+
+	cols, rows, resolution_setting := Width, Height, 0b10
+
+	d.sendCommand(TRES, []byte{
+		byte(cols >> 8),
+		byte(cols & 0x0F),
+		byte(rows >> 8),
+		byte(rows & 0x0F),
+	})
+
+	d.sendCommand(PSR, []byte{
+		byte(resolution_setting << 6 | 0b101111),
+		0x08,
+	})
+
+	d.sendCommand(PWR, []byte{
+		(0x6 << 3) | (0x01 << 2) | (0x01 << 1) | (0x01),
+		0x00,
+		0x23,
+		0x23,
+	})
+
+	d.sendCommand(PLL, []byte{
+		0x3C,
+	})
+
+	d.sendCommand(TSE, []byte{
+		0x00,
+	})
+	d.sendCommand(CDI, []byte{
+		0x1 << 5 | 0x17,
+	})
+	d.sendCommand(TCON, []byte{
+		0x22,
+	})
+	d.sendCommand(DAM, []byte{
+		0x00,
+	})
+	d.sendCommand(PWS, []byte{
+		0xAA,
+	})
+	d.sendCommand(PFS, []byte{
+		0x00,
+	})
+
+}
+
+func (d *Display) SetPixel(x int, y int, color byte) {
+
+	pos := int(Width / 2) * y + int(x / 2)
+	
+	if x & 1 == 0 {
+		// set high bits
+		d.buffer[pos] = (d.buffer[pos] & 0x0F) | ((color & 0x0F) << 4)
+	} else {
+		// set low bits
+		d.buffer[pos] = (d.buffer[pos] & 0xF0) | (color & 0x0F)
+	}
+
+	// log.Printf("d.buffer[%v] = %v", pos, d.buffer[pos])
 }
 
 func (d *Display) Fill() {
-	for i := range d.buffer {
-		d.buffer[i] = 0x2 & 0x07
+	for y := 0; y < 400; y++ {
+		for x := 0; x < 640; x++ {
+			d.SetPixel(x, y, 6)
+		}
 	}
 }
 
 func (d *Display) busyWait(milliseconds int) {
 	duration := time.Duration(milliseconds) * time.Millisecond
+
+	log.Printf("start busy wait")
 
 	if (d.busy.Read() == gpio.High) {
 		// device is not busy
@@ -141,23 +215,32 @@ func (d *Display) busyWait(milliseconds int) {
 		}
 	}
 
+	log.Printf("end busy wait")
 }
 
-func (d *Display) sendCommand(cmd byte, data *[]byte) {
+func (d *Display) sendCommand(cmd byte, data []byte) {
 
-	buf := make([]byte, 1)
-	buf[0] = cmd
-	log.Printf("cmd = %v", cmd)
-	d.spiWrite(SPI_COMMAND, &buf)
+	buf := []byte{ cmd }
+	log.Printf("cmd = 0x%X", cmd)
+	d.spiWrite(SPI_COMMAND, buf)
 
 	if data != nil {
+		// log.Printf("data = %v", data)
 		d.spiWrite(SPI_DATA, data)
 	}
 }
 
 func (d *Display) UpdateScreen() {
+
+	d.setup()
+
 	log.Printf("sending DTM1")
-	d.sendCommand(DTM1, &d.buffer)
+	d.sendCommand(DTM1, d.buffer)
+	d.busyWait(200)
+
+	// log.Printf("sending DSP")
+	// d.sendCommand(DSP, nil)
+	// d.busyWait(200)
 
 	log.Printf("sending PON")
 	d.sendCommand(PON, nil)
@@ -172,18 +255,32 @@ func (d *Display) UpdateScreen() {
 	d.busyWait(200)
 }
 
-func (d *Display) spiWrite(dc gpio.Level, data *[]byte) {
+func (d *Display) spiWrite(dc gpio.Level, data []byte) {
 	
-	read := make([]byte, 0)
-
-	// log.Printf("d = %v", d)
-	log.Printf("CS low")
 	d.cs.Out(gpio.Low)
-	log.Printf("dc = %v", dc)
+
 	d.dc.Out(dc)
 
-	d.spiBus.Tx(*data, read)
+	i := 0
 
-	log.Printf("CS High")
+	for {
+
+		var end int
+
+		if (i + SPI_CHUNK_SIZE) > len(data) {
+			end = len(data)
+		} else {
+			end = i + SPI_CHUNK_SIZE
+		}
+
+		// log.Printf("spi TX data: %v", data[i:end])
+		d.spiBus.Tx(data[i:end], nil)
+
+		i += SPI_CHUNK_SIZE
+		if i > len(data) {
+			break
+		}
+	}
+
 	d.cs.Out(gpio.High)	
 }
